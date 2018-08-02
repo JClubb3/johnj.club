0c28fb8cf1f5b4e5cc7afe640ac490dfa0755a30
John Clubb <johnc@cybernetics.com>
2018-08-02 12:43:14 -0400

    Add specific links for nav to context_processor.

--- articles/context_processors.py
+++ articles/context_processors.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
 
-from .models import Article
+from .models import Article, Series
 from johnjclub.settings import SITE_TITLE
 
 def latest_articles(request):
@@ -16,3 +17,27 @@ def latest_articles(request):
 
 def site_title(request):
     return {"site_title": SITE_TITLE}
+
+def wyverns_and_whimsy_link(request):
+    #pylint: disable=E1101
+    try:
+        w = Series.objects.get(name="Wyverns and Whimsy")
+    except ObjectDoesNotExist:
+        return {"wyverns_link": "#"}
+    return {"wyverns_link": w.get_absolute_url()}
+
+def about_me_link(request):
+    #pylint: disable=E1101
+    try:
+        a = Article.objects.get(title="About Me")
+    except ObjectDoesNotExist:
+        return {"about_link": "#"}
+    return {"about_link": a.get_absolute_url()}
+
+def portfolio_link(request):
+    #pylint: disable=E1101
+    try:
+        a = Article.objects.get(title="Portfolio")
+    except ObjectDoesNotExist:
+        return {"portfolio_link": "#"}
+    return {"portfolio_link": a.get_absolute_url()}
