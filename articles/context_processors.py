5826294eb46e3665519c39cada861d9ff6156446
John Clubb <johnc@cybernetics.com>
2018-08-08 16:32:17 -0400

    Allow latest_articles to error without crash if no articles exist.

--- articles/context_processors.py
+++ articles/context_processors.py
@@ -6,11 +6,15 @@ from johnjclub.settings import SITE_TITLE
 
 def latest_articles(request):
     #pylint: disable=E1101
-    latest_articles = Article.objects.all().filter(
-        enabled=True,
-        publish_date__lte = timezone.now()
-    )[:5]
-    latest_article = latest_articles[0]
+    try:
+        latest_articles = Article.objects.all().filter(
+            enabled=True,
+            publish_date__lte = timezone.now()
+        )[:5]
+        latest_article = latest_articles[0]
+    except IndexError:
+        latest_articles = ""
+        latest_article = ""
 
     return {"latest_articles": latest_articles, 
             "latest_article": latest_article}
