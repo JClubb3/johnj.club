534a5d328f6e4dd3d6e933e71ab51432f89dccaa
John Clubb <johnc@cybernetics.com>
2018-08-10 16:13:19 -0400

    Revert "Revert "Docstrings and help_texts, some small refactoring.""
    This reverts commit 75549ea2783c1848cde3bff748b17f6f123781a6.

--- articles/context_processors.py
+++ articles/context_processors.py
@@ -1,16 +1,26 @@
 from django.utils import timezone
 from django.core.exceptions import ObjectDoesNotExist
+from django.http.request import HttpRequest
+from django.conf import settings
 
 from .models import Article, Series
-from johnjclub.settings import SITE_TITLE
 
-def latest_articles(request):
+def latest_articles(request: HttpRequest) -> dict:
+    """
+    Gets the newest five articles and the absolute newest article.
+    
+    Args:
+        request (HttpRequest): The incoming request.
+
+    Returns:
+        dict: A dict containing the the latest articles and latest article, 
+            available as `latest_articles` and `latest_article`, respectively, 
+            if they exist. Otherwise each are empty strings.
+    """
+
     #pylint: disable=E1101
     try:
-        latest_articles = Article.objects.all().filter(
-            enabled=True,
-            publish_date__lte = timezone.now()
-        )[:5]
+        latest_articles = Article.get_available_articles()[:5]
         latest_article = latest_articles[0]
     except IndexError:
         latest_articles = ""
@@ -19,10 +29,38 @@ def latest_articles(request):
     return {"latest_articles": latest_articles, 
             "latest_article": latest_article}
 
-def site_title(request):
-    return {"site_title": SITE_TITLE}
+def site_title(request: HttpRequest) -> dict:
+    """
+    Adds `SITE_TITLE` from settings to context.
+    
+    Args:
+        request (HttpRequest): The incoming request.
+    
+    Returns:
+        dict: A dict containing the The SITE_TITLE, available as `site_title`.
+    """
 
-def wyverns_and_whimsy_link(request):
+    return {"site_title": settings.SITE_TITLE}
+
+def wyverns_and_whimsy_link(request: HttpRequest) -> dict:
+    """
+    Adds the link to the `Wyverns and Whimsy` series.
+
+    This, the `about_met_link`, and the `portfolio_link` context processors
+    are highly idiomatic to this project. They would probably be better
+    implemented as some sort of iterator in settings, and have the nav and
+    context processors add them in dynamically. But this works for my use
+    case.
+    
+    Args:
+        request (HttpRequest): The incoming request.
+    
+    Returns:
+        dict: A dict containing the URL of the Wyverns and Whimsy link,
+            available as `wyverns_link`, if it exists. Otherwise the value is
+            an octothorpe.
+    """
+
     #pylint: disable=E1101
     try:
         w = Series.objects.get(name="Wyverns and Whimsy")
@@ -30,7 +68,18 @@ def wyverns_and_whimsy_link(request):
         return {"wyverns_link": "#"}
     return {"wyverns_link": w.get_absolute_url()}
 
-def about_me_link(request):
+def about_me_link(request: HttpRequest) -> dict:
+    """
+    Adds the link to the "About Me" article.
+    
+    Args:
+        request (HttpRequest): The incoming request.
+    
+    Returns:
+        dict: A dict containing the "About Me" link, available as 
+            `about_link`, if it exists. Otherwise an octothorpe.
+    """
+
     #pylint: disable=E1101
     try:
         a = Article.objects.get(title="About Me")
@@ -38,7 +87,18 @@ def about_me_link(request):
         return {"about_link": "#"}
     return {"about_link": a.get_absolute_url()}
 
-def portfolio_link(request):
+def portfolio_link(request: HttpRequest) -> dict:
+    """
+    Adds the link to the "Portfolio" article.
+    
+    Args:
+        request (HttpRequest): The incoming request.
+    
+    Returns:
+        dict: A dict containing the "Portfolio" link, available as 
+            `portfolio_link`, if it exists. Otherwise an octothorpe.
+    """
+
     #pylint: disable=E1101
     try:
         a = Article.objects.get(title="Portfolio")
